0af6ab40f956dc0721fa6a031899620f28ece07f
pepeworm <53125684+impyworm@users.noreply.github.com>
2020-08-27 14:21:19 -0600

    Complete challenge one

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
